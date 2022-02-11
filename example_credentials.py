591b2d5c7b56cdbbc789d5151d40b9b168078bb1
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2022-02-11 20:42:01 +1300

    Create example_credentials.py

---
+++ example_credentials.py
@@ -0,0 +1,14 @@
+# Core Credentials, for your school account, raw strings
+email = r"@newlands.school.nz"
+password = r""
+
+# What form class are you in?
+form_class = "13BW"
+
+# Link to the study sign in form. Or your test form.
+form_link = ""
+
+
+# This is intended for testing purposes on different systems. Insert a raw FULL PATH to the chromedriver file. On Windows, include '.exe'
+linux_driver_path = r'/home/'
+windows_driver_path = r"C:\...\chromedriver.exe"
